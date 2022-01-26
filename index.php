e0667856cb979cfa0dc38790be01932f5fd773ac
Thomas Begue <beguethomas63@gmail.com>
2022-01-26 12:39:43 +0100

    initial commit

--- index.php
+++ index.php
@@ -1,14 +1,39 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/styles.css">
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css" integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7" crossorigin="anonymous">
     <title>OhMyFood</title>
 </head>
+
 <body>
+    <header class="header">
+        <h1 class="header__titre">
+            ohmyfood
+        </h1>
+    </header>
+    <main>
+        <section class="map">
+            <i class="fas fa-map-marker-alt map__icon"></i>
+            <span class="map__text">Paris, Belleville</span>
+        </section>
+        
+        <section class="discovery">
+            <h3 class="discovery__title">Réservez le menu qui vous convient</h3>
+            <p class="discovery__text">Découvrez des restaurants d'exception, séléctionnés par nos soins.</p>
+            <button class="discovery__btn">Explorer nos restaurants</button>
+        </section>
+
+
+
+
+    </main>
+    <script src="https://kit.fontawesome.com/e6a1b3e5f7.js" crossorigin="anonymous"></script>
 
 </body>
+
 </html>
